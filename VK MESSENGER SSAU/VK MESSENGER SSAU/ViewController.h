c088b66274fd0c120194b4bf2c5df32209f34d49
Ilya Glazunov <ilyaglazunov@Vladislavs-Mac-mini-2.local>
2016-12-10 13:06:56 +0400

    Создание проекта

---
+++ VK MESSENGER SSAU/VK MESSENGER SSAU/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VK MESSENGER SSAU
+//
+//  Created by Ilya Glazunov on 10.12.16.
+//  Copyright © 2016 Ilya Glazunov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VK MESSENGER SSAU/VK MESSENGER SSAU/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VK MESSENGER SSAU
+//
+//  Created by Ilya Glazunov on 10.12.16.
+//  Copyright © 2016 Ilya Glazunov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
